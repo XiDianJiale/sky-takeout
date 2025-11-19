907cf2eddbffdd92233a99d95b4b2ab2a47ac556
XiDianJiale <savilahao@gmail.com>
2025-11-19 19:55:57 +0800

    订单：用户支付（绕过微信支付）

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
 
 
 @Mapper
@@ -13,4 +17,26 @@ public interface OrderMapper {
      */
     void insert(Orders orders);
 
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+
+    /**
+     * 替换真实的微信支付更新支付数据库状态
+     * @param orderStatus
+     * @param orderPaidStatus
+     */
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus},checkout_time = #{check_out_time} " +
+            "where number = #{orderNumber}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time,String orderNumber);
 }
