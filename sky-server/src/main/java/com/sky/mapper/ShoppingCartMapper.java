00afa711c141cf61e7aa6bece1a799b5a04e76ed
XiDianJiale <savilahao@gmail.com>
2025-11-19 14:29:29 +0800

    购物车：添加

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,39 @@
+package com.sky.mapper;
+
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+
+    /**
+     * 查询购物车列表
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+
+    /**
+     * 根据id修改购物车中商品数量
+     * @param shoppingCart
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart(user_id,dish_id,setmeal_id,name,image,amount,number,create_time,dish_flavor) " +
+            "values(#{userId},#{dishId},#{setmealId},#{name},#{image},#{amount},#{number},#{createTime},#{dishFlavor})")
+    void insert(ShoppingCart shoppingCart);
+}
